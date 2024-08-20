a8bc79dcb675e7f8df80d1a5ec03916c9522b017
Hana00661 <eng.hana95@gmail.com>
2024-08-21 02:25:56 +0400

    0-basic_cache.py

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""0-basic_cache.py
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """a function that inherits from BaseCaching and is a caching system
+    """
+
+    def put(self, key, item):
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        return self.cache_data.get(key) if key is not None else None
