3a4fcaa672083bedfe2d88d79ed2a2311781248a
Hana00661 <eng.hana95@gmail.com>
2024-08-21 15:31:47 +0400

    caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""1-fifo_cache.py
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    a class that inherits from BaseCaching and is a caching system
+    """
+
+    def __init__(self):
+        """
+        Initialize the class with the parent's init method
+        """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """
+        Cache a key-value pair
+        """
+        if key is None or item is None:
+            pass
+        else:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.order[0]))
+                del self.cache_data[self.order[0]]
+                del self.order[0]
+            self.order.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Return the value linked to a given key, or None
+        """
+        if key is not None and key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None
